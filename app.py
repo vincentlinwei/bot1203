b714d9699370f8e5b3e8c90ea73c3a38e5cb609b
vincentlinwei <111618073+vincentlinwei@users.noreply.github.com>
2024-12-08 08:01:06 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -44,6 +44,25 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = text=event.message.text
+    stickers = [
+        {"package_id": "446", "sticker_id": "1988"},
+        {"package_id": "446", "sticker_id": "1989"},
+        {"package_id": "446", "sticker_id": "1990"},
+        {"package_id": "446", "sticker_id": "1991"},
+        {"package_id": "446", "sticker_id": "1992"},
+    ]
+
+    if event.message.text:
+        # 隨機選擇一個貼圖
+        sticker = random.choice(stickers)
+        sticker_message = StickerSendMessage(
+            package_id=sticker["package_id"],
+            sticker_id=sticker["sticker_id"]
+        )
+        line_bot_api.reply_message(event.reply_token, sticker_message)
+    else:
+        reply_text = '很抱歉，我目前無法理解這個內容。'
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(reply_text))
     if message == '天氣':
             reply_text = '請稍等，我幫您查詢天氣資訊！'
             line_bot_api.reply_message(event.reply_token, TextSendMessage(reply_text))
